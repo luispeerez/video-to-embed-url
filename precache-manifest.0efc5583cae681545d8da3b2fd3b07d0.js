900c2904ad1696233d31c933734be2ad76aef813
Luis Perez <luis19prz@gmail.com>
2020-06-13 16:17:28 -0500

    Updates

---
+++ precache-manifest.0efc5583cae681545d8da3b2fd3b07d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a04362640cedccb0970194bf8322a6c3",
+    "url": "/video-to-embed-url/index.html"
+  },
+  {
+    "revision": "523ee70102b382733ffb",
+    "url": "/video-to-embed-url/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c32f62c273a53edc772d",
+    "url": "/video-to-embed-url/static/js/2.dde88ad5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/video-to-embed-url/static/js/2.dde88ad5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "523ee70102b382733ffb",
+    "url": "/video-to-embed-url/static/js/main.ef8a0278.chunk.js"
+  },
+  {
+    "revision": "7991fdcbe28c26397b6a",
+    "url": "/video-to-embed-url/static/js/runtime-main.28f46bb9.js"
+  }
+]);
